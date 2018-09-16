a407c018ec419536f9f9184c1485c237ffd26619
markdan58 <mark.dan@strathmore.edu>
2018-09-16 23:15:51 +0300

    adding the third if statement

--- scripts.js
+++ scripts.js
@@ -8,4 +8,7 @@ if (side1 == side2 && side2 == side3 && side1 == side3) {
         alert("Isoceles Triangle Exactly two sides equal");
     }
     else
-    
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+            alert("Not a triangle");
+        }
+        else    
