2ec98f00b259b7cca3815e2de17d74fea318c464
markdan58 <mark.dan@strathmore.edu>
2018-09-16 23:14:12 +0300

    adding first if statement

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 var side1 = parseInt(prompt("Enter side 1"));
 var side2 = parseInt(prompt("Enter side 2"));
 var side3 = parseInt(prompt("Enter side 3"));
+if (side1 == side2 && side2 == side3 && side1 == side3) {
+    alert("Equilateral Triangle All sides Equal");
+} else
