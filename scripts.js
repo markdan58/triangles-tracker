9ee201c140ab62c7523f0c1931210cf08336e987
markdan58 <mark.dan@strathmore.edu>
2018-09-16 23:16:45 +0300

    closing the if else statement

--- scripts.js
+++ scripts.js
@@ -11,4 +11,7 @@ if (side1 == side2 && side2 == side3 && side1 == side3) {
         if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
             alert("Not a triangle");
         }
-        else    
+        else
+        {
+            alert("scalene");
+        }    
