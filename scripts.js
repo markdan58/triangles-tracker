02a7af9573d8dd2c4201160e5dac60803e092760
markdan58 <mark.dan@strathmore.edu>
2018-09-16 23:15:04 +0300

    adding the second if statement

--- scripts.js
+++ scripts.js
@@ -4,3 +4,8 @@ var side3 = parseInt(prompt("Enter side 3"));
 if (side1 == side2 && side2 == side3 && side1 == side3) {
     alert("Equilateral Triangle All sides Equal");
 } else
+    if (side1 == side2 && side2 !== side3 && side1 !== side3) {
+        alert("Isoceles Triangle Exactly two sides equal");
+    }
+    else
+    
