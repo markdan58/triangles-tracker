f7b9762bc01913ad1d89478da46a1ee200a019f8
markdan58 <mark.dan@strathmore.edu>
2018-09-17 11:00:31 +0300

    changing of prompt

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
-var side1 = parseInt(prompt("Enter side 1"));
-var side2 = parseInt(prompt("Enter side 2"));
-var side3 = parseInt(prompt("Enter side 3"));
-if (side1 == side2 && side2 == side3 && side1 == side3) {
-    alert("Equilateral Triangle All sides Equal");
-} else
-    if (side1 == side2 && side2 !== side3 && side1 !== side3) {
-        alert("Isoceles Triangle Exactly two sides equal");
+function tracker(){
+    var side1 = parseInt(document.getElementById("slide1").value);
+    var side2 = parseInt(document.getElementById("slide2").value);
+    var side3 = parseInt(document.getElementById("slide3").value);
+    var display = document.getElementById("display");
+    
+    if (side1 == side2 && side2 == side3 && side1 == side3){
+       display.innerHTML = "Equilateral Triangle All sides Equal";
+    } else
+    if (side1 == side2 && side2 !== side3 && side1 !== side3){
+       display.innerHTML = "Isoceles Triangle Exactly two sides equal";
+    } else
+    if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2){
+        display.innerHTML = "Not a triangle";
+    } else 
+    {
+        display.innerHTML = "scalene";
     }
-    else
-        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
-            alert("Not a triangle");
-        }
-        else
-        {
-            alert("scalene");
-        }    
+}
